7ec7cca728021dcab7c879942eeabcb7e1ee34bd
immarketing <immarketing@mail.ru>
2016-01-19 00:25:11 +0300

    555

--- SafetySchool/documents/readyhtml/Gruntfile.js
+++ SafetySchool/documents/readyhtml/Gruntfile.js
@@ -5,6 +5,46 @@ module.exports = function(grunt) {
 	grunt
 			.initConfig({
 				pkg : grunt.file.readJSON('package.json'),
+				copy: {
+					
+					  main: {
+					    files: [
+					      // includes files within path
+					      {expand: true, src: ['bower_components/**/dist/**'], dest: 'build/' /*, filter: 'isFile'*/}
+					      /*
+					      ,
+
+					      // includes files within path and its sub-directories
+					      {expand: true, src: ['path/**'], dest: 'dest/'},
+
+					      // makes all src relative to cwd
+					      {expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
+
+					      // flattens results to a single level
+					      {expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'},
+					      */
+					    ],
+					  },
+					  
+					},				
+				bower_concat: {
+					  all: {
+					    dest: 'build/components.js',
+					    cssDest: 'build/components.css',
+					    exclude: [
+					      //'jquery',
+					      //'modernizr'
+					    ],
+					    //dependencies: {
+					      //'underscore': 'jquery',
+					      //'backbone': 'underscore',
+					      //'jquery-mousewheel': 'jquery'
+					    //},
+					    bowerOptions: {
+					      relative: false
+					    }
+					  }
+					},				
 				watch : {
 					styles : {
 						files : [ 'less/**/*.less' ], // which files to watch
@@ -17,8 +57,8 @@ module.exports = function(grunt) {
 				less : {
 					development : {
 						options : {
-							compress : true,
-							yuicompress : true,
+							compress : false,
+							yuicompress : false,
 							optimization : 2
 						},
 						files : {
@@ -36,12 +76,18 @@ module.exports = function(grunt) {
 					}
 				}
 			});
+	
+	
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-bower-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	// Default task(s).
-	grunt.registerTask('default', [ 'uglify','less', 'watch'  ]);
+	//grunt.registerTask('default', [ 'uglify','less', 'watch'  ]);
+	//grunt.registerTask('default', [ 'uglify','less', 'bower_concat'  ]);
+	grunt.registerTask('default', ['uglify','less', 'copy']);
 	
 	// 11
 
