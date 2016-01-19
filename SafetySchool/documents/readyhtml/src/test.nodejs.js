00f58192d47aa3958ac96e715e33b76e88c03fbc
AVGorbunov <immarketing@yandex.ru>
2016-01-19 16:19:49 +0300

    11

--- SafetySchool/documents/readyhtml/src/test.nodejs.js
+++ SafetySchool/documents/readyhtml/src/test.nodejs.js
@@ -1,3 +1,74 @@
 // asdfsadfsadfs
 
-function d(){};
+function d() {
+};
+
+function getTree() {
+	// Some logic to retrieve, or generate tree structure
+	var tree = [ {
+		text : "Parent 1",
+		nodes : [ {
+			text : "Child 1",
+			nodes : [ {
+				text : "Grandchild 1"
+			}, {
+				text : "Grandchild 2"
+			} ]
+		}, {
+			text : "Child 2"
+		} ]
+	}, {
+		text : "Parent 2"
+	}, {
+		text : "Parent 3"
+	}, {
+		text : "Parent 4"
+	}, {
+		text : "Parent 5"
+	}, {
+		text : "Parent 2"
+	}, {
+		text : "Parent 3"
+	}, {
+		text : "Parent 4"
+	}, {
+		text : "Parent 5"
+	}, {
+		text : "Parent 2"
+	}, {
+		text : "Parent 3"
+	}, {
+		text : "Parent 4"
+	}, {
+		text : "Parent 5"
+	}, {
+		text : "Parent 2"
+	}, {
+		text : "Parent 3"
+	}, {
+		text : "Parent 4"
+	}, {
+		text : "Parent 5"
+	}, {
+		text : "Parent 2"
+	}, {
+		text : "Parent 3"
+	}, {
+		text : "Parent 4"
+	}, {
+		text : "Parent 5"
+	} ];
+	return tree;
+}
+
+$('#tree').treeview({
+	data : getTree()
+});
+
+$('#tree').on('nodeSelected', function(event, data) {
+	// Your logic goes here
+	console.log( "You clicked a paragraph!" );
+	console.log( event );
+	console.log( data );
+	$('div#itemIdHolder').html(data.nodeId);
+});
