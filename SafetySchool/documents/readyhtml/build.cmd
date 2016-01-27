41d0c3d7d69c1f5aa8aeea46fd8dfa4c90de0a99
AVGorbunov <immarketing@yandex.ru>
2016-01-27 08:21:11 +0300

    qqqqq

--- SafetySchool/documents/readyhtml/build.cmd
+++ SafetySchool/documents/readyhtml/build.cmd
@@ -1,2 +1,5 @@
-lessc ./less/algo.less > algo.css
+rem lessc ./less/algo.less > algo.css
 rem lessc ./node_modules/bootstrap/less/bootstrap.less > bootstrap.css
+cmd /C npm update
+cmd /C bower update
+cmd /C composer update
